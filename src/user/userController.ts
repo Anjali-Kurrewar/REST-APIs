6f2ad36b0027fd2a452789158066b3c6475d238f
Anjali Kurrewar <anjali.kurrewar1234@gmail.com>
2024-09-21 15:44:35 -0700

    Add resgister user validatin

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,14 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
+    const{name, email, password} = req.body;
+    console.log("reqdata", req.body);
+    //Validation
+    if(!name||!email||!password) {
+        const error = createHttpError(400, "All fields are required");
+        return next(error);
+    }
     res.json({message: "User created"});
 }
 
