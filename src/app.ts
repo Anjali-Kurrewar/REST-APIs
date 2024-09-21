06a6aaff82037d8d0a5754374de60a68fde92f60
Anjali Kurrewar <anjali.kurrewar1234@gmail.com>
2024-09-21 14:48:49 -0700

    globalerror handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
-import express from 'express';
+import express, {Request,Response, NextFunction} from 'express';
+import createHttpError, { HttpError } from 'http-errors';
+import { config } from './config/config';
+import globalErrorHandler from './middleware/globalErrorHandler';
  
 const app = express();
 
 
 //Router
-app.get('/',(req, res) => {
+app.get('/',(req, res, next) => {
+    const error = createHttpError(400,"Something went wrong");
+    throw error;
     res.json({message: "Welcome to the elib apis"})
 });
+
+//Global error handler
+
+app.use(globalErrorHandler);
 
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ conf();
 const _config = {
     port : process.env.PORT,
     datbaseUrl: process.env.MONGO_CONNECTION_STRING,
+    env: process.env.NODE_ENV,
 };
 
 export const config = Object.freeze(_config);
