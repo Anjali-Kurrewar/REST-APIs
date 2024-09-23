2415d4a6d6c0364ef825c5771ad2540f9624aacb
Anjali Kurrewar <anjali.kurrewar1234@gmail.com>
2024-09-22 19:21:08 -0700

    add password hash

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
-import userModel from "./userModel";              
+import userModel from "./userModel";  
+import bcrypt from "bcrypt";            
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const{name, email, password} = req.body;
@@ -15,6 +16,10 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         const error = createHttpError(400, "User already exist with this email.")
         return next(error);
     }
+    //Password Hashing
+    // by using salt which is basically a string which is used to insert in the hased password field so that the patterns where undetectable by any hacker.
+    const hashedPassword = await bcrypt.hash(password,10);
+
     res.json({message: "User created"});
 }
 
